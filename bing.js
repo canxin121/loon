10ea52e013e768a6ff39f7c00d21a8a4c246ede8
canxin121 <69547456+canxin121@users.noreply.github.com>
2023-03-25 16:36:41 +0800

    Update bing.js

--- bing.js
+++ bing.js
@@ -1,15 +1,15 @@
-  const reqHeaders = $request.headers;
+  const headers = $request.headers;
 
-  reqHeaders['x-forwarded-for'] = '1.1.1.1';
+  headers['x-forward-for'] = '1.1.1.1';
 
-  delete reqHeaders["user-agent"];
-  delete reqHeaders["sec-ch-ua-full-version"];
-  delete reqHeaders["sec-ch-ua-full-version-list"];
-  let userAgent = 'iPhone';
+  delete headers["user-agent"];
+  delete headers["sec-ch-ua-full-version"];
+  delete headers["sec-ch-ua-full-version-list"];
+  let ua = 'iPhone';
 
-  reqHeaders['User-Agent'] = `Mozilla/5.0 (${userAgent}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
-  reqHeaders['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
-  reqHeaders['sec-ch-ua-mobile'] = '?0'
-  reqHeaders['sec-ch-ua-platform'] = 'macOS'
+  headers['User-Agent'] = `Mozilla/5.0 (${ua}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
+  headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
+  headers['sec-ch-ua-mobile'] = '?0'
+  headers['sec-ch-ua-platform'] = 'macOS'
 
-  $done({ headers: reqHeaders });
+  $done({ headers });
