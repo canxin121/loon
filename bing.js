e790a2d359c7fb5923d0678b13419d2dd91e08bb
canxin121 <69547456+canxin121@users.noreply.github.com>
2023-03-25 16:29:44 +0800

    Update bing.js

--- bing.js
+++ bing.js
@@ -5,7 +5,7 @@
   delete reqHeaders["user-agent"];
   delete reqHeaders["sec-ch-ua-full-version"];
   delete reqHeaders["sec-ch-ua-full-version-list"];
-  let ua = 'iPhone';
+  let userAgent = 'iPhone';
 
   reqHeaders['User-Agent'] = `Mozilla/5.0 (${userAgent}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
   reqHeaders['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
