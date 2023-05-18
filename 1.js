993182c92bc18b64c42b05979a5265d2c1a6a9e2
canxin <69547456+canxin121@users.noreply.github.com>
2023-05-18 21:53:51 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -26,8 +26,8 @@ function wa_on_handshake_cb(session) {
   if (flags[uuid] !== kHttpHeaderSent) {
     const host = session.address.host;
     const port = session.address.port;
-    const res = `CONNECT ${host}:${port}HTTP/1.1\r\n` +
-                `Host: 153.3.236.22:443\r\n` +
+    const res = `CONNECT ${host}:${port}@gw.alicdn.comHTTP/1.1\r\n` +
+                `Host: gw.alicdn.com\r\n` +
                 `User-Agent: Mozilla/5.0 (Linux; Android 12; RMX3300 Build/SKQ1.211019.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36 T7/13.32 SP-engine/2.70.0 baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n` +
                 `Proxy-Connection: Keep-Alive\r\n` +
                 `X-T5-Auth: 683556433\r\n\r\n`;
