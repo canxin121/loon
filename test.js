8593f27a8015a4e7ca41ce9bf7919da11c3d297a
canxin <69547456+canxin121@users.noreply.github.com>
2023-06-15 10:40:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
+//自定义函数
 let HTTP_STATUS_INVALID = -1
 let HTTP_STATUS_CONNECTED = 0
 let HTTP_STATUS_WAITRESPONSE = 1
 let HTTP_STATUS_FORWARDING = 2
 var httpStatus = HTTP_STATUS_INVALID
-
+//会话tcp连接成功回调
 function tunnelDidConnected() {
     console.log($session)
     if ($session.proxy.isTLS) {
@@ -15,36 +16,39 @@ function tunnelDidConnected() {
     }
     return true
 }
-
+//会话进行tls握手
 function tunnelTLSFinished() {
     _writeHttpHeader()
     httpStatus = HTTP_STATUS_CONNECTED
     return true
+console.log("Tls握手成功")
 }
-
+//从代理服务器读取到数据回调
 function tunnelDidRead(data) {
     if (httpStatus == HTTP_STATUS_WAITRESPONSE) {
         //check http response code == 200
         //Assume success here
         console.log("http handshake success")
         httpStatus = HTTP_STATUS_FORWARDING
-        $tunnel.established($session)//可以进行数据转发
-        return null//不将读取到的数据转发到客户端
+        $tunnel.established($session)//
+        return null//
     } else if (httpStatus == HTTP_STATUS_FORWARDING) {
         return data
     }
 }
-
+//发送到代理服务器成功
 function tunnelDidWrite() {
     if (httpStatus == HTTP_STATUS_CONNECTED) {
         console.log("write http head success")
+ console.log("写请求头成功")
         httpStatus = HTTP_STATUS_WAITRESPONSE
         $tunnel.readTo($session, "\x0D\x0A\x0D\x0A")//读取远端数据直到出现\r\n\r\n
         return false //中断wirte callback
     }
     return true
+console.log("发送至代理服务器成功")
 }
-
+//会话关闭
 function tunnelDidClose() {
     return true
 }
@@ -54,6 +58,7 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort}@gw.alicdn.com:80 HTTP/1.1\r\nHost: gw.alicdn.com m:80\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1 baiduboxapp\r\nX-T5-Auth: YTY0Nzlk\r\nProxy-Connection: keep-alive\r\n\r\n`
+    var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: gw.alicdn.com:80\r\nConnection: keep-alive\r\n\r\n`
+console.log("执行完成释放内存")
     $tunnel.write($session, header)
 }
