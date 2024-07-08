6d3dcf435502dfb5267d070fae04e48ef3092152
canxin <69547456+canxin121@users.noreply.github.com>
2024-07-08 08:30:50 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -6,7 +6,7 @@ let HTTP_STATUS_FORWARDING = 2
 var httpStatus = HTTP_STATUS_INVALID
 //会话tcp连接成功回调
 function tunnelDidConnected() {
-    console.log($session)
+    // console.log($session)
     if ($session.proxy.isTLS) {
         //https
     } else {
@@ -21,14 +21,14 @@ function tunnelTLSFinished() {
     _writeHttpHeader()
     httpStatus = HTTP_STATUS_CONNECTED
     return true
-console.log("Tls握手成功")
+// console.log("Tls握手成功")
 }
 //从代理服务器读取到数据回调
 function tunnelDidRead(data) {
     if (httpStatus == HTTP_STATUS_WAITRESPONSE) {
         //check http response code == 200
         //Assume success here
-        console.log("http handshake success")
+        // console.log("http handshake success")
         httpStatus = HTTP_STATUS_FORWARDING
         $tunnel.established($session)//
         return null//
@@ -39,14 +39,14 @@ function tunnelDidRead(data) {
 //发送到代理服务器成功
 function tunnelDidWrite() {
     if (httpStatus == HTTP_STATUS_CONNECTED) {
-        console.log("write http head success")
- console.log("写请求头成功")
+        // console.log("write http head success")
+ // console.log("写请求头成功")
         httpStatus = HTTP_STATUS_WAITRESPONSE
         $tunnel.readTo($session, "\x0D\x0A\x0D\x0A")//读取远端数据直到出现\r\n\r\n
         return false //中断wirte callback
     }
     return true
-console.log("发送至代理服务器成功")
+// console.log("发送至代理服务器成功")
 }
 //会话关闭
 function tunnelDidClose() {
@@ -59,6 +59,6 @@ function _writeHttpHeader() {
     let conPort = $session.conPort
 
     var header = `CONNECT ${conHost}:${conPort}@gw.alicdn.com:80HTTP/1.1\r\nHost: gw.alicdn.com:80\r\nConnection: keep-alive\r\n\r\n`
-console.log("执行完成释放内存")
+// console.log("执行完成释放内存")
     $tunnel.write($session, header)
 }
